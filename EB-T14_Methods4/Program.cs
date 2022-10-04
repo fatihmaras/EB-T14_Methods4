b46cb356cecc6957bca127a7e18728af24626f49
fatihmaras <fatihmmaras@gmail.com>
2022-10-04 03:59:10 +0300

    Methods Example

---
+++ EB-T14_Methods4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodlarOrnek_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Bir Sayı Giriniz: ");
+            int GirilenSayi = int.Parse(Console.ReadLine());
+
+            TekmiCiftmi(GirilenSayi);
+            Console.ReadLine();
+
+        }
+
+        static void TekmiCiftmi(int sayi)
+        {
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("Sayı çifttir...");
+            }
+            else
+            {
+                Console.WriteLine("Sayı Tektir...");
+
+            }
+
+        }
+    }
+}
+
